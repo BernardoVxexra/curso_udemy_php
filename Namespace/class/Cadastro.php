b8fe9b3302bc59e2c6d34262547f60c207a56b0d
BernardoVxexra <bernardo.santos13@etec.sp.gov.br>
2024-10-11 09:21:50 -0300

    Add getters e setters

--- Namespace/class/Cadastro.php
+++ Namespace/class/Cadastro.php
@@ -10,6 +10,12 @@
      public function getNome():string{
         return $this->nome;//Chamando o atributo nome pelo dolar this
      }
+     public function getEmail():string{
+        return $this->email;
+     }
+     public function getSenha():string{
+        return $this->senha;
+     }
 
  }
 
