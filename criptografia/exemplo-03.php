202273ecab6b5ec20facf20d6ad04b687f384bd8
BernardoVxexra <bernardo.santos13@etec.sp.gov.br>
2024-10-14 19:09:42 -0300

    finalizado

--- criptografia/exemplo-03.php
+++ criptografia/exemplo-03.php
@@ -1,5 +1,18 @@
 <?php
 
+$cadastro = [
+    "nome" => "Taligado" //Valor associado
+];//Esses serão os dados a serem criptografados
 
+
+define('SECRET', 'senha1234567890');
+
+function decrypt($cadastro, $key) {
+    list($encryptedData, $iv) = explode('::', base64_decode($cadastro), 2);
+    return json_decode(openssl_decrypt($encryptedData, 'AES-128-CBC', $key, 0, $iv), true);
+}
+
+$decrypted = decrypt($encrypted, SECRET);
+echo "Dados descriptografados: " . print_r($decrypted, true);
 
 ?>
