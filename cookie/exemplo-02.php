df7724d6c443f328e852880e98c183cda19b29be
BernardoVxexra <bernardo.santos13@etec.sp.gov.br>
2024-10-14 17:36:53 -0300

    criptografia

--- cookie/exemplo-02.php
+++ cookie/exemplo-02.php
@@ -1,3 +1,15 @@
 <?php
-  
+if (isset($_COOKIE["NOME_DO_COOKIE"])) {
+    // Decodifica o cookie JSON
+    $obj = json_decode($_COOKIE["NOME_DO_COOKIE"], true);
+    
+    // Verifica se a decodificação foi bem-sucedida e se a chave "empresa" existe
+    if (is_array($obj) && isset($obj['empresa'])) {
+        echo $obj['empresa'];  
+    } else {
+        echo "A chave 'empresa' não foi encontrada.";
+    }
+} else {
+    echo "O cookie não está definido.";
+}
 ?>
