a74c1dd023fa2f57996c3a6b024b54434d223d3c
BernardoVxexra <bernardo.santos13@etec.sp.gov.br>
2024-10-14 16:07:18 -0300

    dir

---
+++ DIR/exemplo-01.php
@@ -0,0 +1,13 @@
+<?php
+//Criando diretórios 
+$name = "images";
+
+if(!is_dir($name)){
+   mkdir($name);
+   echo "Diretório $name criado com sucesso";
+}else {
+    rmdir($name);
+    echo "Já existe o diretório: $name foi removido " ;
+}
+
+?>
